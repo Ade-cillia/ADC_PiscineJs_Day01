b1beb43ee52367c88e2d191d61f778cb1514e75a
Aurélien <aureliendu3809@outlook.fr>
2021-11-30 16:06:55 +0100

    rush00 fix

--- rush00/program.js
+++ rush00/program.js
@@ -8,13 +8,12 @@ function validIPAdresses(string) {
                 ipSecond = string.slice(i+1,j+1);
                 ipThird = string.slice(j+1,k+1);
                 ipFourth = string.slice(k+1);
-                if (ipFirst < 255 && ipSecond < 255 && ipThird < 255 && ipFourth < 255) {
+                if (ipFirst <= 255 && ipSecond <= 255 && ipThird <= 255 && ipFourth <= 255) {
                     if (ipFirst != "" && ipSecond != "" && ipThird != "" && ipFourth != "") {
                         if (!(ipFirst[1] && ipFirst[0] == 0) && !(ipSecond[1] && ipSecond[0] == 0) && !(ipThird[1] && ipThird[0] == 0) && !(ipFourth[1] && ipFourth[0] == 0)) {
                             ip = ipFirst+"."+ipSecond+"."+ipThird+"."+ipFourth;
                             ipArr.push(ip);
                         }
-                        
                     }
                 }
             }
@@ -23,8 +22,8 @@ function validIPAdresses(string) {
     return ipArr
 }
 
-//const string = '1921680';
-//console.log(validIPAdresses(string));
+const string = '255255255255';
+console.log(validIPAdresses(string));
 
 // Merci de ne pas effacer la ligne en dessous.
 exports.validIPAddresses = validIPAdresses;
