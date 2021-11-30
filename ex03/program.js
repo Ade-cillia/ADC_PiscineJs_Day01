08a3b267e23a12c57dd99cd600efbb1acbf6e4a1
Aurélien <aureliendu3809@outlook.fr>
2021-11-30 13:13:27 +0100

    ex03

---
+++ ex03/program.js
@@ -0,0 +1,21 @@
+function runLengthEncoding(string) {
+    let encodedString="";
+    let nbLetter;
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] == string[i-1]) {
+            nbLetter+=1;
+        } else if (string[i] != string[i-1]){
+            nbLetter=1;
+        }
+        if (nbLetter==9 || !string[i+1] || string[i] != string[i+1]) {
+            encodedString+= (nbLetter==1)?string[i]:nbLetter+string[i]
+            nbLetter=0;
+        }
+    }
+    return encodedString;
+}
+
+const string = "AAAAYAAAAAAAAABBCCCCDDY"
+console.log(runLengthEncoding(string))
+
+exports.runLengthEncoding = runLengthEncoding;
