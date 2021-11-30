211980c7187ba8e20b3598c903301d7604e9b00d
Aurélien <aureliendu3809@outlook.fr>
2021-11-30 17:17:57 +0100

    ex02 remind

--- ex02/program.js
+++ ex02/program.js
@@ -1,15 +1,57 @@
 function pocketMoney(coins){
-    coins.sort((a,b)=> a-b);
-    let addNum=0;
-    for (let i = 0; i < coins.length; i++) {
-        if ( coins[i] > addNum+1) {
-            return addNum+1
-        }
-        addNum+=coins[i];
+    let objectifFind = {
+        "stop":false,
+        "objectif":0
+    };
+    let testNum = false;
+    let sumAll= coins.reduce((a,b)=> a+b);
+    
+    for (let i = 1; i <= sumAll; i++) {
+        
+        if (!objectifFind["stop"]) {
+            testNum = false
+            for (let j = 0; j < coins.length; j++) {
+                if (!testNum) {
+                    objectifFind = searchCombine(coins.filter((_,key) => key !== j),0,i)
+                    if (!objectifFind["stop"]) {
+                        testNum=true;
+                    }
+                }
+                
+            }
+            console.log(objectifFind)
+        } else if(objectifFind["stop"]) {
+            return objectifFind["objectif"];
+        }    
     }
-    return addNum+1
+    return sumAll+1;
 }
-//let coins = [1,2,3,4,5];
+function searchCombine(array,sum,objectif){
+    let remontada = false;
+    console.log(array);
+    for (let i = 0; i < array.length; i++) {
+        sum=sum+array[i]
+        console.log("sum="+ (sum) );
+        if (sum == objectif) {
+            console.log("sum=oui");
+            remontada=true;
+            return {
+                "stop":false,
+                "objectif":objectif
+            };
+        }
+    }
+    if (array.length == 1 && !remontada) {
+        return {
+            "stop":true,
+            "objectif":objectif
+        };
+    } 
+    sum=0
+    return searchCombine(array.filter((_,key) => key !== 0),sum,objectif);
+}
+
+//let coins = [ 1,2,3,4,5];
 //console.log(pocketMoney(coins));
 
 exports.pocketMoney =  pocketMoney;
